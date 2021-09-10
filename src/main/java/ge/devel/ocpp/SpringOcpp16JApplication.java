d5c1c0b4572dbc79e4184722a77aa03c2bba677d
kalosha <mikheil.kalandadze@itcs.ge>
2021-09-10 19:12:58 +0400

    Initial commit

---
+++ src/main/java/ge/devel/ocpp/SpringOcpp16JApplication.java
@@ -0,0 +1,13 @@
+package ge.devel.ocpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOcpp16JApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOcpp16JApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ge/devel/ocpp/SpringOcpp16JApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.devel.ocpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOcpp16JApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
